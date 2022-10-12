a1defb581231d2571d1878e39b0c1863c28b21ce
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-12 12:41:47 +0300

    Example003 'Random' added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int num1 = 10;
-int num2 = 5;
+﻿int num1 = new Random().Next(1, 10); //случайное число в диапозоне 1 2 3 ...9 (мин всегда включается в диапозон, а макс нет)
+Console.WriteLine(num1); 
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
 Console.WriteLine(num1 + num2);
 
 
