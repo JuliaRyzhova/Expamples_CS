2134588c4858c6eb8e40aa53e1edffcae770ce93
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-12 13:21:07 +0300

    Condition 'If-Else' project added

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "julia")
+{
+    Console.WriteLine("Привет, моя дорогая Julia");
+}
+else
+{
+    Console.Write("Ну привет, давно не виделись! ");
+    Console.WriteLine(username);
+}
