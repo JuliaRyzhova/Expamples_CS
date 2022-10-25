55b9f3f2f7d32dd36b1953104daf378e103d080c
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-25 10:17:24 +0300

    sort array from max to min

---
+++ Example016_SortArray(max-min)/Program.cs
@@ -0,0 +1,30 @@
+﻿// Сортировка массива в обратном порядке (от макс к мин)
+
+int [] array ={3, 8 , 4 , 5 , 5 , 3 , 10 , 2 , 7};
+
+void PrintArray (int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int [] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition= j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+SelectionSort(array);
+PrintArray (array);
