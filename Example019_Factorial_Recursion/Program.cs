77534372ddccfedc9ceaca2e40740b61a1a5bc3c
Julia Ryzhova <16-j.lazy@bk.ru>
2022-11-13 14:54:38 +0300

    factorial  recursion

---
+++ Example019_Factorial_Recursion/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial(int number) // int не может хранить такие большие числа
+// поэтому случается переполнение типа. Можно изменить тип на double
+{
+    if (number == 1) return 1;
+    else return number* Factorial(number -1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"Factorial {i} {Factorial(i)}");
+}
